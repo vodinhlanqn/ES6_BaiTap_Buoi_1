731983b2b98e0f142706a32fc8ca18ce3e489ce6
Vo Dinh Lan <vodinhlanqn@gmail.com>
2022-08-29 15:32:44 +0700

    Add For/in For/of

--- baitapSpread/js/main.js
+++ baitapSpread/js/main.js
@@ -2,9 +2,28 @@ let contentText = document.querySelector('.heading').innerHTML;
 let spreadText = [...contentText];
 
 let spanHTML = '';
-spreadText.forEach(function (item) {
+// Cách 1: sử dụng For of
+for (var item of spreadText) {
     spanHTML += `
         <span>${item}</span>
     `;
-});
+    console.log(item);
+}
 document.querySelector('.heading').innerHTML = spanHTML;
+
+// Cách 2: sử dụng For in
+// for (var item in spreadText) {
+//     spanHTML += `
+//         <span>${spreadText[item]}</span>
+//     `;
+// }
+// document.querySelector('.heading').innerHTML = spanHTML;
+
+// Cách 3 sử dụng forEach
+// spreadText.forEach(function (item) {
+//     spanHTML += `
+//         <span>${item}</span>
+//     `;
+// });
+// document.querySelector('.heading').innerHTML = spanHTML;
+
